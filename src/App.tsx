a9106ad6f9c1b88f03f93ada050b8a8d055eb121
Jethro Coma <jethrocoma@Jethros-MacBook-Pro.local>
2024-10-27 12:02:16 -0700

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+/*
+  Components that I will need for this project
+    -Navigation component
+    -LogIn/Register
+    -Input components for text input
+    -Weather cards.
+    
+  Things to consider besides how the wireframe will look for this app. 
+    -What the data structure will look like for the users
+    -What the weather cards will look like
+    -figuring out how NOAA CO-OPS endpoints work 
+*/
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <></>;
+}
+
+export default App;
